2ef35df5e3f6bfc996cb376d0137d7be8b3284d6
Yoonsoyoung02 <yoonsoyoung56@gmail.com>
2025-01-19 22:30:05 +0900

    [Silver V] Title: 너의 평점은, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,31 +1,20 @@
-
 grade_to_point = {
-    "A+": 4.5, "A0": 4.0,
-    "B+": 3.5, "B0": 3.0,
-    "C+": 2.5, "C0": 2.0,
-    "D+": 1.5, "D0": 1.0,
-    "F": 0.0
-}
+    "A+":4.5,"A0":4.0,
+    "B+":3.5,"B0":3.0,
+    "C+":2.5,"C0":2.0,
+    "D+":1.5,"D0":1.0,
+    "F":0.0
+    }
 
-
-subjects = []
-for _ in range(20):
-    data = input().strip().split()
-    subject_name, credit, grade = data[0], float(data[1]), data[2]
-    subjects.append((credit, grade))
-
-weighted_sum = 0.0
 credit_sum = 0.0
+total_cg = 0.0
 
-for credit, grade in subjects:
-    if grade == "P":
+for _ in range(20):
+    subject_name, credit, grade = input().split()
+    credit = float(credit)
+    if grade == 'P':
         continue
-    weighted_sum += credit * grade_to_point[grade]
+    total_cg = total_cg + credit*grade_to_point[grade]
     credit_sum += credit
 
-
-if credit_sum > 0:
-    major_gpa = weighted_sum / credit_sum
-    print(f"{major_gpa:.6f}")
-else:
-    print("0.000000")
+print(total_cg/credit_sum)
