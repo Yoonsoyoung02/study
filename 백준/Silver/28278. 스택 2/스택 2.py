4f0aa942f3c52ee8e1b0b362fe83b6c6f2997a54
Yoonsoyoung02 <yoonsoyoung56@gmail.com>
2025-02-03 11:01:37 +0900

    [Silver IV] Title: 스택 2, Time: 1244 ms, Memory: 72404 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,25 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+num_list=[]
+
+for i in range(N):
+    com = list(map(int, sys.stdin.readline().strip().split()))
+
+    if com[0] == 1:
+        num_list.append(com[1])
+
+    elif com[0] == 5:
+        if len(num_list) == 0:
+            print(-1)
+        else:
+            print(num_list[-1])
+
+    elif com[0] == 3 :
+        print(len(num_list))
+
+    elif com[0] == 2:
+        print(num_list.pop() if num_list else -1)
+
+    elif com[0] == 4:
+        print(1 if not num_list else 0)
